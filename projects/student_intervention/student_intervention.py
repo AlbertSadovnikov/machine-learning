36b09af10b306886d257c5fb06716aaa953e9d4e
Albert Sadovnikov <albert.sadovnikov@gmail.com>
2017-01-17 13:46:40 +0200

    Project 3 progress

--- projects/student_intervention/student_intervention.py
+++ projects/student_intervention/student_intervention.py
@@ -1,41 +1,35 @@
-"""
-Script for testing different classifiers on the data set.
-    Gaussian Naive Bayes (GaussianNB)
-    Decision Trees
-    Ensemble Methods (Bagging, AdaBoost, Random Forest, Gradient Boosting)
-    K-Nearest Neighbors (KNeighbors)
-    Stochastic Gradient Descent (SGDC)
-    Support Vector Machines (SVM)
-    Logistic Regression
-"""
-
 # Import libraries
+import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from utils import preprocess_features, train_predict
-from sklearn.naive_bayes import GaussianNB
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import BaggingClassifier, RandomForestClassifier, AdaBoostClassifier
-from sklearn.linear_model import SGDClassifier, LogisticRegression
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.model_selection import KFold, GridSearchCV
-from sklearn.metrics import accuracy_score
+from time import time
+from sklearn.metrics import f1_score
+import sys
 
 # Read student data
-student_data = pd.read_csv('student-data.csv')
+student_data = pd.read_csv("student-data.csv")
+print "Student data read successfully!"
+
+# TODO: Calculate number of students
 n_students = len(student_data)
+
+# TODO: Calculate number of features
 n_features = len(student_data.columns) - 1
+
+# TODO: Calculate passing students
 n_passed = len(student_data[student_data['passed'] == 'yes'])
+
+# TODO: Calculate failing students
 n_failed = len(student_data[student_data['passed'] == 'no'])
+
+# TODO: Calculate graduation rate
 grad_rate = float(n_passed) / (n_passed + n_failed)
 
 # Print the results
-print("Total number of students: {}".format(n_students))
-print("Number of features: {}".format(n_features))
-print("Number of students who passed: {}".format(n_passed))
-print("Number of students who failed: {}".format(n_failed))
-print("Graduation rate of the class: {:.2f}%".format(grad_rate))
+print "Total number of students: {}".format(n_students)
+print "Number of features: {}".format(n_features)
+print "Number of students who passed: {}".format(n_passed)
+print "Number of students who failed: {}".format(n_failed)
+print "Graduation rate of the class: {:.2f}%".format(grad_rate * 100)  # output is intended to be in precents
 
 # Extract feature columns
 feature_cols = list(student_data.columns[:-1])
@@ -44,144 +38,127 @@ feature_cols = list(student_data.columns[:-1])
 target_col = student_data.columns[-1]
 
 # Show the list of columns
-print("Feature columns:\n{}".format(feature_cols))
-print("Target column: {}".format(target_col))
+print "Feature columns:\n{}".format(feature_cols)
+print "\nTarget column: {}".format(target_col)
 
 # Separate the data into feature data and target data (X_all and y_all, respectively)
 X_all = student_data[feature_cols]
 y_all = student_data[target_col]
 
+# Show the feature information by printing the first five rows
+print "\nFeature values:"
+print X_all.head()
+
+
+def preprocess_features(X):
+    ''' Preprocesses the student data and converts non-numeric binary variables into
+        binary (0/1) variables. Converts categorical variables into dummy variables. '''
+
+    # Initialize new output DataFrame
+    output = pd.DataFrame(index=X.index)
+
+    # Investigate each feature column for the data
+    for col, col_data in X.iteritems():
+
+        # If data type is non-numeric, replace all yes/no values with 1/0
+        if col_data.dtype == object:
+            col_data = col_data.replace(['yes', 'no'], [1, 0])
+
+        # If data type is categorical, convert to dummy variables
+        if col_data.dtype == object:
+            # Example: 'school' => 'school_GP' and 'school_MS'
+            col_data = pd.get_dummies(col_data, prefix=col)
+
+            # Collect the revised columns
+        output = output.join(col_data)
+
+    return output
+
+
 X_all = preprocess_features(X_all)
-print("Processed feature columns ({} total features):\n{}".format(len(X_all.columns), list(X_all.columns)))
+print "Processed feature columns ({} total features):\n{}".format(len(X_all.columns), list(X_all.columns))
 
-# do the split
-X_train, X_test, y_train, y_test = train_test_split(X_all, y_all, test_size=0.33, random_state=42)
+# TODO: Import any additional functionality you may need here
+from sklearn.cross_validation import train_test_split
 
-print('\n\n********** classifiers **************\n')
+# TODO: Set the number of training points
+num_train = 300
 
-# Naive Bayes, no extra parameters
-clf = GaussianNB()
-clf.fit(X_train, y_train)
-y_p = clf.predict(X_test)
-acc_score = accuracy_score(y_test, y_p)
-print('Gaussian NB accuracy: %.4f\n' % acc_score)
+# Set the number of testing points
+num_test = X_all.shape[0] - num_train
 
-# Decision tree
-clf = DecisionTreeClassifier(random_state=0)
-clf.fit(X_train, y_train)
-y_p = clf.predict(X_test)
-acc_score = accuracy_score(y_test, y_p)
-print('default decision tree accuracy: %.4f' % acc_score)
-
-# parameter search for decision tree
-dt = DecisionTreeClassifier()
-param_grid = {"criterion": ["gini", "entropy"],
-              "min_samples_split": range(2, 100, 2)}
-
-clf = GridSearchCV(dt, param_grid)
-clf.fit(X_train, y_train)
-bp = clf.best_params_
-print('estimated parameters')
-for k, v in bp.items():
-    print("\t{:<20s}: {}".format(k, v))
-
-clf = DecisionTreeClassifier(**bp, random_state=0)
-clf.fit(X_train, y_train)
-y_p = clf.predict(X_test)
-acc_score = accuracy_score(y_test, y_p)
-print('Decision tree accuracy: %.4f\n' % acc_score)
-
-# Bagging with decision tree
-dt = DecisionTreeClassifier(random_state=0)
-clf = BaggingClassifier(base_estimator=dt, n_estimators=100, random_state=0)
-clf.fit(X_train, y_train)
-y_p = clf.predict(X_test)
-acc_score = accuracy_score(y_test, y_p)
-print('default Bagging with decision tree accuracy: %.4f\n' % acc_score)
-
-# Bagging with decision tree
-gnb = GaussianNB()
-clf = BaggingClassifier(base_estimator=gnb, n_estimators=100, random_state=0)
-clf.fit(X_train, y_train)
-y_p = clf.predict(X_test)
-acc_score = accuracy_score(y_test, y_p)
-print('default Bagging with gnb: %.4f\n' % acc_score)
-
-# random forest
-clf = RandomForestClassifier(random_state=0)
-clf.fit(X_train, y_train)
-y_p = clf.predict(X_test)
-acc_score = accuracy_score(y_test, y_p)
-print('default random forest: %.4f\n' % acc_score)
-
-rf = RandomForestClassifier(random_state=0)
-param_grid = {"criterion": ["gini", "entropy"],
-              "min_samples_split": range(2, 100, 2)}
-clf = GridSearchCV(rf, param_grid)
-clf.fit(X_train, y_train)
-bp = clf.best_params_
-print('estimated parameters')
-for k, v in bp.items():
-    print("\t{:<20s}: {}".format(k, v))
-
-clf = RandomForestClassifier(**bp, random_state=0)
-clf.fit(X_train, y_train)
-y_p = clf.predict(X_test)
-acc_score = accuracy_score(y_test, y_p)
-print('Random forest accuracy: %.4f\n' % acc_score)
-
-# random forest
-clf = AdaBoostClassifier(random_state=0)
-clf.fit(X_train, y_train)
-y_p = clf.predict(X_test)
-acc_score = accuracy_score(y_test, y_p)
-print('default AdaBoost: %.4f\n' % acc_score)
-
-# KNN
-clf = KNeighborsClassifier(n_neighbors=2)
-clf.fit(X_train, y_train)
-y_p = clf.predict(X_test)
-acc_score = accuracy_score(y_test, y_p)
-print('default knn: %.4f\n' % acc_score)
-
-# SGD
-clf = SGDClassifier()
-clf.fit(X_train, y_train)
-y_p = clf.predict(X_test)
-acc_score = accuracy_score(y_test, y_p)
-print('default sgdc: %.4f\n' % acc_score)
-
-# SVM
-clf = SVC()
-clf.fit(X_train, y_train)
-y_p = clf.predict(X_test)
-acc_score = accuracy_score(y_test, y_p)
-print('default svm: %.4f\n' % acc_score)
-
-svm = SVC()
-param_grid = [
-  {'C': [1, 10, 100, 1000], 'kernel': ['linear']},
-  {'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.0001], 'kernel': ['rbf']},
- ]
-clf = GridSearchCV(svm, param_grid)
-clf.fit(X_train, y_train)
-bp = clf.best_params_
-print('estimated parameters')
-for k, v in bp.items():
-    print("\t{:<20s}: {}".format(k, v))
-
-clf = SVC(**bp)
-clf.fit(X_train, y_train)
-y_p = clf.predict(X_test)
-acc_score = accuracy_score(y_test, y_p)
-print('SVM accuracy: %.4f\n' % acc_score)
+# TODO: Shuffle and split the dataset into the number of training and testing points above
+X_train, X_test, y_train, y_test = train_test_split(X_all, y_all, test_size=num_test, random_state=0)
+# Show the results of the split
+print "Training set has {} samples.".format(X_train.shape[0])
+print "Testing set has {} samples.".format(X_test.shape[0])
 
 
-# Logistic regression
-clf = LogisticRegression()
-clf.fit(X_train, y_train)
-y_p = clf.predict(X_test)
-acc_score = accuracy_score(y_test, y_p)
-print('default logistic regression: %.4f\n' % acc_score)
+# modified versions, to return values
+
+def train_classifier(clf, X_train, y_train):
+    ''' Fits a classifier to the training data. '''
+
+    # Start the clock, train the classifier, then stop the clock
+    start = time()
+    clf.fit(X_train, y_train)
+    end = time()
+
+    # Print the results
+    # print("Trained model in {:.4f} seconds".format(end - start))
+    return end - start
 
 
+def predict_labels(clf, features, target):
+    ''' Makes predictions using a fit classifier based on F1 score. '''
+
+    # Start the clock, make predictions, then stop the clock
+    start = time()
+    y_pred = clf.predict(features)
+    end = time()
+
+    # Print and return results
+    # print "Made predictions in {:.4f} seconds.".format(end - start)
+    # sys.stdout.write('| prt %.4f |' % (end - start))
+    return f1_score(target.values, y_pred, pos_label='yes'), end - start
+
+
+def train_predict(clf, X_train, y_train, X_test, y_test):
+    ''' Train and predict using a classifer based on F1 score. '''
+
+    # Indicate the classifier and the training set size
+    # print "Training a {} using a training set size of {}. . .".format(clf.__class__.__name__, len(X_train))
+    # Train the classifier
+    tr_time = train_classifier(clf, X_train, y_train)
+
+    # Print the results of prediction for both training and testing
+    # print "F1 score for training set: {:.4f}.".format(predict_labels(clf, X_train, y_train))
+    # print "F1 score for test set: {:.4f}.".format(predict_labels(clf, X_test, y_test))
+    f1_train, _ = predict_labels(clf, X_train, y_train)
+    f1_test, pr_time = predict_labels(clf, X_test, y_test)
+    sys.stdout.write('| %d | %.6f | %.6f | %.4f | %.4f |\n' % (len(X_train), tr_time, pr_time, f1_train, f1_test))
+
+
+# TODO: Import the three supervised learning models from sklearn
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from itertools import product
+
+# TODO: Initialize the three models
+clf_A = DecisionTreeClassifier(random_state=0)
+clf_B = RandomForestClassifier(random_state=0)
+clf_C = SVC(random_state=0)
+
+
+# TODO: Execute the 'train_predict' function for each classifier and each training set size
+for idx, clf in enumerate([clf_A, clf_B, clf_C], start=1):
+    sys.stdout.write('** Classifier %d - %s **\n\n' % (idx, clf.__class__.__name__))
+    sys.stdout.write('| Training Set Size | Training Time | Prediction Time (test) ')
+    sys.stdout.write('| F1 Score (train) | F1 Score (test) |\n')
+    sys.stdout.write('| :---------------: | :---------------------: | :--------------------: ')
+    sys.stdout.write('| :--------------: | :-------------: |\n')
+    for train_set_size in [100, 200, 300]:
+        train_predict(clf, X_train[:train_set_size], y_train[:train_set_size], X_test, y_test)
+
+    sys.stdout.write('\n\n')
